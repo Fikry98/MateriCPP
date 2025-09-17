b4f159c454bc5d68a5f85c5c51d69756b733b7fa
Fikry98 <111934860+Fikry98@users.noreply.github.com>
2025-09-17 14:15:39 +0700

    Add files via upload

---
+++ Praktikum5/535250184_Code1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    int i;
+
+    cout<<"Isi dari array indeks ke  adalah "<<cars[0]<<endl<<endl;
+    cout<<"Mencetak isi dari array cars"<<endl;
+
+    for(i=0; i<4; i++)
+    {
+        cout<<"Cars index ke "<<i<<" = "<<cars[i]<<endl;
+    }
+
+    return 0;
+}
---
+++ Praktikum5/535250184_Code5.cpp
@@ -0,0 +1,48 @@
+//Program untuk menghitung persentase jawaban responden dari suatu survei
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char jawaban [41];
+    int i, N;
+    float nY=0, nT=0., nA=0, nI=0., pY=0, pT=0, pA=0, pI=0;
+    cout<<"Ketikkan jumlah data: ";
+    cin>>N;
+    cout<<"Ketikkan huruf (Y/T/A/y/t/a):";
+    for (i=0; i<N; i++)
+    {
+        cin>>jawaban[i];
+    }
+    // Blok if-else di bawah ini seharusnya berada di dalam for loop
+    if(jawaban[i] == 'Y' || jawaban[i] == 'y')
+    {
+        nY++;
+    }
+    else if(jawaban[i] == 'T' || jawaban[i] == 't')
+    {
+        nT++;
+    }
+    else if(jawaban [i] == 'A' || jawaban [i] == 'a')
+    {
+        nA++;
+    }
+    else
+    {
+        nI++;
+    }
+
+    pY=nY/N*100;
+    pT=nT/N*100;
+    pA=nA/N*100;
+    pI=nI/N*100;
+
+    cout<<endl;
+    cout<<"Persentase jawaban Y atau y adalah "<<pY<<"%"<<endl;
+    cout<<"Persentase jawaban T atau t adalah "<<pT<<"%"<<endl;
+    cout<<"Persentase jawaban A atau a adalah "<<pA<<"%"<<endl;
+    cout<<"Persentase jawaban yang tidak valid "<<pI<<"%"<<endl;
+
+    return 0;
+}
